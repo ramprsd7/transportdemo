1529204852e2df0af6619b551444082fc9b3bc48
Ram Prasad <ram@truckishlabs.com>
2022-01-21 22:35:21 +0530

    first commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,26 +8,32 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  form : FormGroup;
+  form: FormGroup;
   submitted = false;
   autocomplete: google.maps.places.Autocomplete;
-  
+  tripArray = [];
+  tripForm: any;
+
 
   constructor(
-    private router : Router,
+    private router: Router,
     private formBuilder: FormBuilder,
   ) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.loadTripForm();
+  }
 
 
-    tripForm = new FormGroup({
-      startpoint : new FormControl("",[Validators.required]),
-      endPoint : new FormControl("",[Validators.required])
+  loadTripForm() {
+    this.tripForm = new FormGroup({
+      startpoint: new FormControl("", [Validators.required]),
+      endpoint: new FormControl("", [Validators.required])
     });
-    
+  }
 
-  
+
+
 
   get startPoint() {
     return this.tripForm.get('startPoint');
@@ -37,23 +43,59 @@ export class HomeComponent implements OnInit {
     return this.tripForm.get('endPoint');
   }
 
-  onSubmit(){
+  submit() {
     if (this.tripForm.invalid) {
       alert('Form is invalid kindly input details required')
-    }else{
-      alert('Reactive form submited!')
-      console.log(this.tripForm);
+    } else {
+      let trip = this.tripForm.value;
+
+      this.tripArray.push(trip.startpoint + ' - ' + trip.endpoint);
+
       console.log(this.tripForm.value);
+      console.log(this.tripArray);
     }
   }
 
-  
+  swapsource() {
+    let currentValue = this.tripForm.value
+    this.tripForm.get('startpoint').patchValue(currentValue.endpoint);
+    this.tripForm.get('endpoint').patchValue(currentValue.startpoint);
+  }
 
-  setGoogleAutoComplete(event) {
+
+
+  setStartGoogleAutoComplete(event) {
+    this.autocomplete = new google.maps.places.Autocomplete(event.target, {
+      types: ['(cities)'],
+      componentRestrictions: { 'country': 'IN' }
+    });
+
+    this.autocomplete.addListener('place_changed', () => {
+      const place: google.maps.places.PlaceResult = this.autocomplete.getPlace();
+
+      if (place.geometry === undefined || place.geometry === null) {
+        return;
+      }
+
+      const city = place.address_components.filter(address => address.types.includes('locality'));
+      const state = place.address_components.filter(address =>
+        address.types.includes('administrative_area_level_1')
+      );
+
+      const strip = {
+        city: city.length > 0 ? city[0]['long_name'] : '',
+        state: state.length > 0 ? state[0]['long_name'] : '',
+      }
+      console.log(strip);
+      this.tripForm.get('startpoint').patchValue(strip.city.toLocaleUpperCase().slice(0, 3));
+    });
+  }
+
+  setEndGoogleAutoComplete(event) {
     // this.autocomplete = new google.maps.places.Autocomplete(event.target);
-    this.autocomplete = new google.maps.places.Autocomplete(event.target ,{
-      types : ['(cities)'],
-      componentRestrictions: {'country' : 'IN'}
+    this.autocomplete = new google.maps.places.Autocomplete(event.target, {
+      types: ['(cities)'],
+      componentRestrictions: { 'country': 'IN' }
     });
     this.autocomplete.addListener('place_changed', () => {
       const place: google.maps.places.PlaceResult = this.autocomplete.getPlace();
@@ -65,13 +107,15 @@ export class HomeComponent implements OnInit {
       const state = place.address_components.filter(address =>
         address.types.includes('administrative_area_level_1')
       );
-      const trip = {
+      const ecity = {
         city: city.length > 0 ? city[0]['long_name'] : '',
         state: state.length > 0 ? state[0]['long_name'] : '',
       }
-      console.log(trip);
-      
+      console.log(ecity);
+      this.tripForm.get('endpoint').patchValue(ecity.city.toLocaleUpperCase().slice(0, 3));
+
     });
   }
+
 }
 
